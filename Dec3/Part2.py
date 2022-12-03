de6e8425214c6a54c6490074cd88e424fe046c84
Tom Fish <guerillero.wikipedia@gmail.com>
2022-12-03 11:03:38 +0100

    improve computation time

--- Dec3/Part2.py
+++ Dec3/Part2.py
@@ -2,12 +2,12 @@ import re
 
 data = open("Data.txt")
 
+basket = []
 triples = []
 count = 0
 
 for line in data:
     count += 1
-    basket = []
     if count == 1:
         elf1 = line.strip()
     elif count == 2:
@@ -15,12 +15,11 @@ for line in data:
     elif count == 3:
         count = 0
         for charater in line.strip():
-            if elf1.find(charater) > -1:
-                if elf2.find(charater) > -1:
-                    if re.search('[a-z]', charater):
-                        basket.append(ord(charater) - 96)
-                    else:
-                        basket.append(ord(charater) - 38)
+            if elf1.find(charater) > -1 and elf2.find(charater) > -1:
+                if re.search('[a-z]', charater):
+                    basket.append(ord(charater) - 96)
+                else:
+                    basket.append(ord(charater) - 38)
         triples.append(basket[0])
         basket = []
 
